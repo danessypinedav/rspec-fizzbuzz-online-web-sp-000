832011e49a2cc9e89cb09b576f263fa68abf3141
chic-creator-9382 <danessypinedav@gmail.com>
2020-08-17 13:06:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -15,3 +15,16 @@ end
 end 
 
   
+def fizzbuzz(int)
+  if (int % 5 == 0) && (int % 3 == 0)
+puts "Fizzbuzz"
+elsif (int % 3 == 0)
+puts "Fizz"
+elsif (int % 5 == 0)
+puts "Buzz"
+else
+puts "nil"
+end 
+
+ 
+end
